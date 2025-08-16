761ca3eecf14ef53e11fd2c03f29aa318a8d8c3e
Lunna5 <git@lunna.dev>
2025-08-16 11:58:16 +0200

    refactor: rename event handler functions for consistency

--- test/event.test.c
+++ test/event.test.c
@@ -36,25 +36,25 @@ void on_key_press_handler_2(const EventContainer* e) {
 int main(void) {
 	log_set_level(LOG_DEBUG);
 	event_system_init();
-	register_event(EVENT_KEY_PRESS, on_key_press_handler_1);
-	register_event(EVENT_KEY_PRESS, on_key_press_handler_2);
+	event_register_handler(EVENT_KEY_PRESS, on_key_press_handler_1);
+	event_register_handler(EVENT_KEY_PRESS, on_key_press_handler_2);
 
 	const EventContainer e1 = {.type = EVENT_KEY_PRESS,
 						 .length = sizeof(KeyPressData),
 						 .data.key_press = {.key = 'A'}};
 
 	log_debug("[EVENT] Triggering EVENT_KEY_PRESS with key = A");
-	trigger_event(&e1);
+	event_trigger(&e1);
 
-	unregister_event(EVENT_KEY_PRESS, on_key_press_handler_1);
+	event_unregister_handler(EVENT_KEY_PRESS, on_key_press_handler_1);
 
 	log_debug("[EVENT] Now only on_key_press_handler_2 should be triggered.");
 
-	trigger_event(&e1); // Should only trigger on_key_press_handler_2
+	event_trigger(&e1); // Should only trigger on_key_press_handler_2
 
-	unregister_event(EVENT_KEY_PRESS, on_key_press_handler_2);
+	event_unregister_handler(EVENT_KEY_PRESS, on_key_press_handler_2);
 
-	trigger_event(&e1);
+	event_trigger(&e1);
 
 	event_system_cleanup();
 
